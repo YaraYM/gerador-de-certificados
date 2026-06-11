7197316ca88b33938c2937751273eb4a32a880b5
Yara Y Marques <yaraym0408@gmail.com>
2026-06-11 17:58:51 -0300

    feat(router): usa routar para navegação via TS

--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
+
 
 @Component({
   selector: 'app-certificados',
